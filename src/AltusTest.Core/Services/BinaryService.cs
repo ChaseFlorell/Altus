eb83769393d125c4177cbd81ec2560cd5c64513c
Chase Florell <chase.florell@gmail.com>
2014-09-07 13:12:18 -0600

    implemented

--- src/AltusTest.Core/Services/BinaryService.cs
+++ src/AltusTest.Core/Services/BinaryService.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AltusTest.Core.Services
 {
@@ -6,12 +8,49 @@ namespace AltusTest.Core.Services
     {
         public string BinCount(decimal input)
         {
-            throw new NotImplementedException();
+            var binaries = new Stack<string>(); // stack is reversed, so we're actually building the collection backwards (counting up).
+
+            for (var i = 0; i <= input; i++)
+            {
+                binaries.Push(DecimalToBin(i));
+            }
+            return string.Join(" ", binaries);
         }
 
-        public bool VerifyBinary(decimal input, string expected)
+        private static string DecimalToBin(decimal number)
         {
-            throw new NotImplementedException();
+            var decimalNumber = (int)number;
+
+            var result = string.Empty;
+            while (decimalNumber > 0)
+            {
+                var remainder = decimalNumber % 2;
+                decimalNumber /= 2;
+                result = remainder + result;
+            }
+
+            return result.PadLeft(2,'0'); // padded to allow for 1 and 0.
+        }
+
+        private static decimal BinToDecimal(string binary)
+        {
+            return Convert.ToInt32(binary, 2); // didn't use ToList() ;-)
+        }
+
+        public bool VerifyBinary(string binaryString, decimal expectedFirstNumber)
+        {
+            var numbers = binaryString.Split(' ');
+            var collection = new decimal[(int)expectedFirstNumber-1];
+
+            for (var i = 0; i <= collection.Length-1; i++)
+            {
+                collection[i] = BinToDecimal(numbers[i]);
+            }
+
+            var numberIsCorrect = collection[0] == expectedFirstNumber;
+            var isSequential = Enumerable.Range(0, collection.Length).Any(i => collection[i] != collection[0] + i);
+
+            return numberIsCorrect && isSequential;
         }
     }
 }
--- src/AltusTest.Tests/Unit/BinaryTests.cs
+++ src/AltusTest.Tests/Unit/BinaryTests.cs
@@ -22,7 +22,7 @@ namespace AltusTest.Tests.Unit
 
             // execute
             var binary = _binaryService.BinCount(original);
-            var verified = _binaryService.VerifyBinary(original, binary);
+            var verified = _binaryService.VerifyBinary(binary, original);
 
             // assert
             Assert.IsTrue(verified);
@@ -37,10 +37,24 @@ namespace AltusTest.Tests.Unit
 
             // execute
             var actual = _binaryService.BinCount(original);
-            var verified = _binaryService.VerifyBinary(original, actual);
+            var verified = _binaryService.VerifyBinary(actual, original);
 
             // assert
             Assert.AreEqual(expected, actual);
+            Assert.IsTrue(verified);
+        }
+
+        [Test]
+        public void ShouldVerifyBinaryString()
+        {
+            // setup
+            const decimal firstNumber = 5;
+            const string binaryString= @"101 100 11 10 01 00";
+
+            // execute
+            var verified = _binaryService.VerifyBinary(binaryString, firstNumber);
+
+            // assert
             Assert.IsTrue(verified);
         }
     }
