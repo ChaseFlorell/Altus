f8b2fd2a85002ee0e43bed8f3e1e16dc09676685
Chase Florell <chase.florell@gmail.com>
2014-09-08 20:37:48 -0600

    more sql stuff

---
+++ sql/CreateCategoriesTable.sql
@@ -0,0 +1,32 @@
+USE [SQLProgrammingTest]
+GO
+
+-- =============================================
+-- Author:		Chase Florell
+-- Create date: 9/7/2014
+-- Description:	
+--      Design a database structure that represents a tree of categories. 
+--      Each category has a name and exactly one parent category. 
+--      Describe using T-SQL syntax how you would retrieve all categories directly under a particular category (all children).
+-- =============================================
+
+CREATE TABLE [dbo].[Categories]
+(
+	[Id] [bigint] IDENTITY(1,1) NOT NULL,
+	[ParentId] [bigint] NULL,
+	[Name] [nvarchar](20) NULL,
+	CONSTRAINT [PK_Categories] PRIMARY KEY CLUSTERED 
+	(
+		[Id] ASC
+	)
+) 
+GO
+
+ALTER TABLE [dbo].[Categories]  WITH CHECK ADD  CONSTRAINT [FK_Categories_Categories] FOREIGN KEY([ParentId])
+REFERENCES [dbo].[Categories] ([Id])
+GO
+
+ALTER TABLE [dbo].[Categories] CHECK CONSTRAINT [FK_Categories_Categories]
+GO
+
+
