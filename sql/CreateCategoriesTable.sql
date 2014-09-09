bd91c002c417b5e06d0c0e3b47a6adaa53d7d647
Chase Florell <chase.florell@gmail.com>
2014-09-09 08:57:48 -0600

    Added the subsequent query

--- sql/CreateCategoriesTable.sql
+++ sql/CreateCategoriesTable.sql
@@ -29,4 +29,21 @@ GO
 ALTER TABLE [dbo].[Categories] CHECK CONSTRAINT [FK_Categories_Categories]
 GO
 
+-- After data has been inserted, query the table
 
+SELECT           [root].[Name] AS [RootCategory]
+                ,[child1].[Name] AS [FirstChildCategory]
+                ,[child2].[Name] AS [SecondChildCategory]
+
+FROM            [Categories] AS [Root]
+
+LEFT OUTER JOIN [Categories] AS [child1] ON  [child1].parentid = [root].id
+LEFT OUTER JOIN [Categories]  AS [child2] ON [child2].parentid = [child1].id
+
+WHERE           [root].[Id] = 1
+
+ORDER BY        RootCategory 
+               ,FirstChildCategory 
+               ,SecondChildCategory 
+
+GO
